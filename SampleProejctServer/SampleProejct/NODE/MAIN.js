977d8620b07455207b3e964192bcb1f2104ffee4
Hanul <hanul@hanul.me>
2017-04-07 22:02:39 +0900

    no message

--- SampleProejctServer/SampleProejct/NODE/MAIN.js
+++ SampleProejctServer/SampleProejct/NODE/MAIN.js
@@ -3,15 +3,25 @@ SampleProejct.MAIN = METHOD({
 	run : (addRequestHandler) => {
 		
 		SampleProejct.ROOM('Sample', (clientInfo, on, off, send, broadcastExceptMe) => {
-			console.log('Sample 룸에 접속하였습니다.')
+			console.log('Sample 룸에 접속하였습니다.');
 			
-			on('message', (data, ret) => {
-				
+			send({
+				methodName : 'messageFromServer',
+				data : {
+					a : 1,
+					b : 2,
+					c : 3
+				}
+			});
+			
+			on('messageFromClient', (data, ret) => {
+				console.log('메시지가 도착하였습니다.', data);
+				ret(data);
 			});
 		});
 		
 		SampleProejct.ROOM('Sample2', (clientInfo, on, off, send, broadcastExceptMe) => {
-			console.log('Sample2 룸에 접속하였습니다.')
+			console.log('Sample2 룸에 접속하였습니다.');
 		});
 	}
 });
